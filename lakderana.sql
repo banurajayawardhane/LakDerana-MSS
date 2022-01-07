4cd30b2ab4f6a177a62f49c0ed86898cd14aea6b
Anushka Wijegoonawardana <anushkaduwolka123@gmail.com>
2022-01-07 08:36:05 +0530

    database

--- lakderana.sql
+++ lakderana.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 02, 2022 at 04:25 PM
+-- Generation Time: Jan 07, 2022 at 04:05 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.3.29
 
@@ -20,6 +20,57 @@ SET time_zone = "+00:00";
 --
 -- Database: `lakderana`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bar_orders`
+--
+
+CREATE TABLE `bar_orders` (
+  `order_id` varchar(255) NOT NULL,
+  `cutomer_id` int(11) NOT NULL,
+  `item_id` varchar(255) NOT NULL,
+  `order_quantity` varchar(255) NOT NULL,
+  `total_price` varchar(255) NOT NULL,
+  `discounts` varchar(255) NOT NULL,
+  `net_price` varchar(255) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bar_orders`
+--
+
+INSERT INTO `bar_orders` (`order_id`, `cutomer_id`, `item_id`, `order_quantity`, `total_price`, `discounts`, `net_price`, `date`) VALUES
+('', 2, '', '2', '2000', '100', '1900', '0000-00-00'),
+('61d772a72f02f', 2, '', '2', '2000', '100', '1900', '2022-01-06'),
+('61d772bb72d97', 1, '', '6', '6000', '1', '5400', '2022-01-06'),
+('61d7730343cf6', 2, '61', '2', '2', '2', '2', '2022-01-06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bar_stocks`
+--
+
+CREATE TABLE `bar_stocks` (
+  `stock_id` int(11) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `item_description` varchar(255) NOT NULL,
+  `item_quntity` varchar(255) NOT NULL,
+  `buying_price` varchar(255) NOT NULL,
+  `selling_price` varchar(255) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bar_stocks`
+--
+
+INSERT INTO `bar_stocks` (`stock_id`, `item_name`, `item_description`, `item_quntity`, `buying_price`, `selling_price`, `date`) VALUES
+(61, 'Testitem', 'zddddddddddddd', '10', '1000', '1100', '2022-01-06'),
+(23, 'testt', 'sZxc', '10', '1000', '1100', '2022-01-07');
 
 -- --------------------------------------------------------
 
@@ -42,7 +93,8 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`id`, `name`, `nic`, `phone`, `email`, `address`, `cuss_id`) VALUES
-(1, 'banura jayawardhane', '200122600054', '0771161599', 'banura@gmail.comj', '23', '2234');
+(1, 'banura jayawardhane', '200122600054', '0771161599', 'banura@gmail.comj', '23', '2234'),
+(2, 'Anushka Diwolka', '000000000v', '0711971313', 'anushkaduwolka123@gmail.com', '1st Lane', '2');
 
 -- --------------------------------------------------------
 
@@ -117,7 +169,8 @@ INSERT INTO `users` (`name`, `id`, `pass`, `email`, `rfid`, `post`, `user_id`) V
 ('banura', 'FO01', '1', '1', '1', 'FO', 1),
 ('nepu', 'RM01', '1', '1', '1', 'RM', 2),
 ('bharana', 'ACC01', '1', '1', '1', 'ACC', 3),
-('radhini', 'RS01', '1', '1', '1', 'RS', 4);
+('radhini', 'RS01', '1', '1', '1', 'RS', 4),
+('barAdmin', 'BM001', '1', '1', '1', 'BM', 5);
 
 --
 -- Indexes for dumped tables
@@ -155,7 +208,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `reservation`
@@ -167,7 +220,7 @@ ALTER TABLE `reservation`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
